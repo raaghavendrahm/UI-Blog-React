6e88155cbbb359b9b78d2254a28ec31d18d70cc4
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-25 13:51:56 +0530

    A button is created in BlogDetails. On clicking it, that blog will be deleted from db.json using DELETE request through fetch. Then with useHistory hook, user is redirected to Home page once blog is deleted.

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -4,6 +4,9 @@ import { useParams } from 'react-router';
 import useFetch from './useFetch';
 // As useFetch is made reusable, the same component is used to display a particular blog by feeding in corresponding url to useFetch.
 
+import { useHistory } from 'react-router';
+// useHistory is used to redirect user from BlogDetails to Home page once blog is deleted.
+
 const BlogDetails = () => {
   // Let's destructure the parameter needed from useParams (id in this case):
   const { id } = useParams();
@@ -18,6 +21,22 @@ const BlogDetails = () => {
   } = useFetch('http://localhost:5000/blogs/' + id);
   // 'id' is obtained from useParams hook and is used in url dynamically to access specific blog.
 
+  // Initialize useHistory():
+  const history = useHistory();
+
+  // Delete Blog
+
+  const handleDelete = () => {
+    fetch('http://localhost:5000/blogs/' + blog.id, {
+      // 'id' value is received in two ways. One from data (blog) from useFetch and other is 'id' from useParams. Here 'blog.id' is used which is from the first way. But, it can be 'id' which is from second way.
+
+      method: 'DELETE',
+    }).then(() => {
+      // Redirect user to Home page once the blog is deleted:
+      history.push('/');
+    });
+  };
+
   return (
     <div className="blog-details">
       {isLoading && <div>Loading...</div>}
@@ -26,6 +45,9 @@ const BlogDetails = () => {
         <h2>{blog.title}</h2>
         <p>Written by {blog.author}</p>
         <div>{blog.body}</div>
+
+        {/* To delete a blog, a button is used. On clicking it, that blog will be deleted from db.json with DELETE request through fetch: */}
+        <button onClick={handleDelete}>Delete Blog</button>
       </article>
     </div>
   );
