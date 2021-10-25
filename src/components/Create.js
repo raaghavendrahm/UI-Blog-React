d5e3322da06a6ae42e79d901fc4975c44149b2db
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-25 11:03:48 +0530

    A form is created to add new blogs. Form value control is done in the next step.

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" required />
+
+        <label>Blog Body:</label>
+        <textarea type="text" required></textarea>
+
+        <label>Blog Author:</label>
+        <select>
+          <option value="raaghu">raaghu</option>
+          <option value="raani">raani</option>
+          <option value="makku">makku</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
